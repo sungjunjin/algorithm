c2397ef5edd9bc9f3e66e7b894a9685746d304a0
sungjunjin <jinsungjun94@gmail.com>
2024-04-10 21:28:45 +0900

    feat: 버블정렬 예시

---
+++ src/main/java/me/sj/algorithm/sort/BubbleSort.java
@@ -0,0 +1,15 @@
+package me.sj.algorithm.sort;
+
+public class BubbleSort {
+    public void sort(int [] intArray) {
+        for (int j = 1; j < intArray.length - 1; j++) {
+            for (int i = 0; i < intArray.length - j; i++) {
+                if (intArray[i] > intArray[i + 1]) {
+                    int temp = intArray[i];
+                    intArray[i] = intArray[i + 1];
+                    intArray[i + 1] = temp;
+                }
+            }
+        }
+    }
+}
