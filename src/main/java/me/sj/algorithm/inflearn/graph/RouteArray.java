9ea76bff57ceda3e4c8ef23459e2c63832a7e967
sungjun <jinsungjun94@gmail.com>
2024-07-03 15:03:51 +0900

    fix: 그래프와 인접행렬 예시 수정

--- src/main/java/me/sj/algorithm/inflearn/graph/RouteArray.java
+++ src/main/java/me/sj/algorithm/inflearn/graph/RouteArray.java
@@ -4,14 +4,30 @@ import java.util.Scanner;
 
 /**
  * 경로탐색 DFS (인접행렬)
+ * 방향그래프가 주어지면 1번 정점에서 N번 정점으로 가는 모든 경로의 가지 수를 출력하는 프로그램을 작성하세요
+ *
+ * 예시)
+ * 5 9
+ * 1 2
+ * 1 3
+ * 1 4
+ * 2 1
+ * 2 3
+ * 2 5
+ * 3 4
+ * 4 2
+ * 4 5
+ * 답 : 6
  */
 public class RouteArray {
+    // 정점의 수
     static int n;
+    // 간선의 수
     static int m;
     static int answer = 0;
-
     static int[][] graph;
     static int[] ch;
+
 
     public static void main(String[] args) {
         RouteArray routeArray = new RouteArray();
@@ -20,50 +36,34 @@ public class RouteArray {
         n = in.nextInt();
         m = in.nextInt();
 
-        graph = new int[n + 1][n + 1];
         ch = new int[n + 1];
+        graph = new int[n + 1][n + 1];
 
         for (int i = 0; i < m; i++) {
-            int a = in.nextInt();
-            int b = in.nextInt();
-            graph[a][b] = 1;
+            int r = in.nextInt();
+            int c = in.nextInt();
+
+            graph[r][c] = 1;
         }
 
         ch[1] = 1;
-        routeArray.solutionLecture(1);
+        routeArray.dfs(1);
 
         System.out.println(answer);
     }
 
-    public void solutionMine(int start) {
-        if (start == n) {
+    public void dfs(int node) {
+        if (node == n) {
             answer++;
         } else {
             for (int i = 1; i <= n; i++) {
-                if(graph[start][i] == 1 && ch[i] == 0) {
+                if (graph[node][i] == 1 && ch[i] == 0) {
                     ch[i] = 1;
-                    solutionMine(i);
+                    dfs(i);
                     ch[i] = 0;
                 }
             }
         }
     }
 
-    public void solutionLecture(int start) {
-        if (start == n) {
-            answer++;
-        } else {
-            for (int i = 1; i <= n; i++) {
-                if (graph[start][i] == 1 && ch[i] == 0) {
-                    // 방문했던 노드 체크
-                    ch[i] = 1;
-
-                    solutionLecture(i);
-
-                    // 방문했던 에서 다시 제외
-                    ch[i] = 0;
-                }
-            }
-        }
-    }
 }
